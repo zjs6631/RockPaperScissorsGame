0ab155367b8ae884dd78e02b9f746a023219c4cf
zjs6631 <zacharyjonessmith@gmail.com>
2022-03-12 15:09:00 -0500

    Added a loop to play 5 rounds at a time

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ function playerSelection(){
     return pSelection
 }
 
-function getRandomIntInclusive(){
+function getRandomIntInclusive(){ //found online to get randint
     let min = Math.ceil(0);
     let max = Math.floor(2);
 
@@ -15,13 +15,16 @@ function getRandomIntInclusive(){
 function computerSelection(){
     const choices = ["rock", "paper", "scissors"]
     
-    console.log(getRandomIntInclusive());
+    console.log(getRandomIntInclusive()); // checking to make sure random int worked
     let cSelection = choices[getRandomIntInclusive()]
     return cSelection
 }
 
+let pWin = 0;
+let cWin = 0;
+
 function playRound(pSelection, cSelection){
-    
+    pSelection = pSelection.toLowerCase()
     console.log(pSelection)
     console.log(cSelection)
 
@@ -29,14 +32,31 @@ function playRound(pSelection, cSelection){
         console.log("It's a tie!")
     } else if ((pSelection == "rock")&&(cSelection == "scissors")){
         console.log("You win! rock beats scissors!")
+        pWin++;
     } else if ((pSelection == "paper")&&(cSelection == "rock")){
         console.log("You win! paper beats rock!")
+        pWin++
     } else if ((pSelection == "scissors")&&(cSelection == "paper")){
         console.log("You win! scissors beats paper!")
+        pWin++
     } else {
         console.log("You lose! try again!")
+        cWin++
     }
     
 }
 
-console.log(playRound(playerSelection(), computerSelection()))
+//console.log(playRound(playerSelection(), computerSelection()))
+
+for (let i = 0; i < 5; i++){
+    playRound(playerSelection(), computerSelection())
+}
+
+console.log(pWin);
+console.log(cWin);
+
+if (pWin > cWin){
+    console.log("You've won the game!")
+} else{
+    console.log("The computer won the game!")
+}
